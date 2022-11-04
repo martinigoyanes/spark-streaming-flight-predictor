9dd66e0d909af9fd61e18fbaefeec6a71a5d292b
Martin Iglesias <martinigoyanes@hotmail.com>
2022-11-04 22:17:41 +0100

    script that creates kafka-mongo-zookeeper

--- kubernetes/create-minikube-cluster.sh
+++ kubernetes/create-minikube-cluster.sh
@@ -4,7 +4,18 @@ minikube start --vm-driver=virtualbox
 
 # Start MongoDB node and load data into it
 minikube kubectl -- apply -f 00-mongodb.yaml
-./setup-mongodb.sh
+
+sleep 20
+MONGODB_POD_UID=$(minikube kubectl -- get pod | grep mongodb | awk '{print $1}')
+minikube kubectl -- wait --for=condition=Ready "pod/${MONGODB_POD_UID}"
+minikube kubectl -- exec -it "pod/${MONGODB_POD_UID}" -- /bin/bash -c "
+apt-get update &&
+apt-get install -y curl &&
+curl -Lko /origin_dest_distances.jsonl http://s3.amazonaws.com/agile_data_science/origin_dest_distances.jsonl &&
+mongoimport --authenticationDatabase=admin --username username --password password --db flight_predictor --collection origin_dest_distances --file origin_dest_distances.jsonl &&
+mongo flight_predictor --eval 'db.origin_dest_distances.createIndex({Origin: 1, Dest: 1})' &&
+mongo --authenticationDatabase=admin --username username --password password flight_predictor --eval 'db.createCollection(\"flight_delay_classification_response\")'
+"
 
 # Start mongo express to easier visualization of data in MongoDB
 minikube kubectl -- apply -f 01-mongo-express.yaml
@@ -13,6 +24,6 @@ minikube kubectl -- apply -f 01-mongo-express.yaml
 minikube kubectl -- apply -f 02-zookeeper.yaml
 
 # Wait for zookeeper to be running before starting Kafka with specific topic
-ZOOKEEPER_POD_UID=$(minikube kubectl -- get pod | grep mongodb | awk '{print $1}')
+ZOOKEEPER_POD_UID=$(minikube kubectl -- get pod | grep zookeeper | awk '{print $1}')
 minikube kubectl -- wait --for=condition=Ready "pod/${ZOOKEEPER_POD_UID}"
 minikube kubectl -- apply -f 03-kafka.yaml
---
+++ kubernetes/frontend/config.py
@@ -0,0 +1,4 @@
+# config.py, a configuration file for index.py
+RECORDS_PER_PAGE=15
+AIRPLANE_RECORDS_PER_PAGE=5
+ELASTIC_URL='http://elastic:9200'
